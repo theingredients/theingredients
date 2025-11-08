4f073c42da7983da774b5d8d7921c89145c3636d
jeromedagustin <theingredientscollective@gmail.com>
2025-11-07 21:47:56 -0800

    Update More page with timeline descriptions

--- src/pages/More.tsx
+++ src/pages/More.tsx
@@ -7,10 +7,10 @@ const More = () => {
       <div className="page-container">
         <h1 className="page-title">Timeline</h1>
         <a href="https://or-six.vercel.app/" target="_blank" rel="noopener noreferrer" className="page-content">
-          The OR (beta)
+          The OR (beta) - audio fun
         </a>
-        <p className="page-content">The DO (alpha)</p>
-        <p className="page-content">The GO (concept)</p>
+        <p className="page-content">The DO (alpha) - digital organizing</p>
+        <p className="page-content">The GO (concept) - geotrack to text</p>
         <p className="page-content">The Future</p>
       </div>
     </Layout>
