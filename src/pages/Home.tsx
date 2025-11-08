4b7ca035d918f0773e44daa28376e1a4d5769a23
jeromedagustin <theingredientscollective@gmail.com>
2025-11-07 20:52:25 -0800

    Fix footer size consistency and add clock to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,34 @@
+import { useState, useEffect } from 'react'
 import Layout from '../components/Layout'
 import './Home.css'
 
 const Home = () => {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+
+    return () => clearInterval(timer)
+  }, [])
+
+  const formatTime = (date: Date) => {
+    return date.toLocaleTimeString('en-US', {
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    })
+  }
+
   return (
     <Layout>
       <div className="home-container">
         <div className="title-stack">
           <h1 className="title-line">The</h1>
           <h1 className="title-line">Ingredients</h1>
+          <div className="clock">{formatTime(time)}</div>
         </div>
       </div>
     </Layout>
